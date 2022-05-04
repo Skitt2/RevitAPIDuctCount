b272a36c9c7a8098662c2387a1fce2011b3e46aa
Skitt2 <provaid@yandex.ru>
2022-05-04 22:12:08 +0300

    Add project files.

---
+++ RevitAPIDuctCount/DuctFilter.cs
@@ -0,0 +1,24 @@
+﻿using Autodesk.Revit.UI.Selection;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.DB.Mechanical;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RevitAPIDuctCount
+{
+    public class DuctFilter : ISelectionFilter
+    {
+        public bool AllowElement(Element elem)
+        {
+            return elem is Duct;
+        }
+
+        public bool AllowReference(Reference reference, XYZ position)
+        {
+            return false;
+        }
+    }
+}
---
+++ RevitAPIDuctCount/Main.cs
@@ -0,0 +1,39 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.DB.Mechanical;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RevitAPIDuctCount
+{
+    [Transaction(TransactionMode.Manual)]
+    public class Main : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            UIApplication uiapp = commandData.Application;
+            UIDocument uidoc = uiapp.ActiveUIDocument;
+            Document doc = uidoc.Document;
+
+            IList<Reference> selectedElementRefList = uidoc.Selection.PickObjects(ObjectType.Element, new DuctFilter(), "Выберите воздуховоды");
+            var ductList = new List<Duct>();
+
+            string info = string.Empty;
+            foreach (var selectedElement in selectedElementRefList)
+            {
+                Duct oDuct = doc.GetElement(selectedElement) as Duct;
+                ductList.Add(oDuct);
+            }
+            info += $"Количество воздуховодов: {ductList.Count}";
+
+            TaskDialog.Show("Selection", info);
+
+            return Result.Succeeded;
+        }
+    }
+}
